d7ebaf34394c02266455a74690dcc1abb12c32d9
Rosemary Kamau <kamaurosemary.n@gmail.com>
2025-04-08 09:23:32 +0300

    Initial commit

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -3,9 +3,6 @@ import { render } from "@testing-library/react";
 import Home from "../components/Home";
 
 test("it is exported as a default export", () => {
-  try {
+
     expect(() => render(<Home />)).not.toThrow();
-  } catch (e) {
-    throw new Error("Make sure to export this component!");
-  }
 });
